2d7b5fe99b683629a92b864e2c83fafac31cd931
hwalborn <hwalborn@gmail.com>
2017-02-24 13:30:39 -0500

    halfway

--- nested_forms/app/controllers/characters_controller.rb
+++ nested_forms/app/controllers/characters_controller.rb
@@ -7,4 +7,15 @@ class CharactersController < ApplicationController
     erb(:'characters/new')
   end
 
+  get '/characters/:id' do
+    @character = Character.find(params[:id])
+    # binding.pry
+    erb :'/characters/show'
+  end
+
+  post '/characters' do
+    @character = Character.create(params)
+    redirect to "/characters/#{@character.id}"
+  end
+
 end
